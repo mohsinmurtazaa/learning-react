3edbf7dd6f280f5cf09d3982326a0bb62d3ec504
Mohsin Murtaza <mohsinmurtaza1402@gmail.com>
2023-11-26 16:52:46 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from mohsin"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
